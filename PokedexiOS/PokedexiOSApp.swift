1aedaccc9579c07a7508d67b38185c324e5ae8a1
Alexander <deep555@hotmail.com>
2025-09-08 13:56:55 +0800

    Initial Commit

---
+++ PokedexiOS/PokedexiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexiOSApp.swift
+//  PokedexiOS
+//
+//  Created by Alexander Chen on 2025/9/8.
+//
+
+import SwiftUI
+
+@main
+struct PokedexiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
